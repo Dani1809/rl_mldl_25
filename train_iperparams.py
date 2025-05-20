22881e98c8773c750f4e9b644d595eb9f9023349
Dani1809 <dani.amato2001@gmail.com>
2025-05-20 19:58:58 +0200

    train e test iperparametri senza udr

--- train_iperparams.py
+++ train_iperparams.py
@@ -5,6 +5,7 @@ import itertools
 from env.custom_hopper import *
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
+import random
 
 SEED = 42
 def set_global_seed(seed):
